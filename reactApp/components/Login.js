12be57cc38bc761eb0d80976ba5c6490fe3add81
tlnguyen4 <tlnguyen@princeton.edu>
2017-08-07 15:51:35 -0700

    splitting up files

---
+++ reactApp/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1 style={{fontFamily: 'Helvetica'}}>Document Share</h1>
+      <p>Log In</p>
+    </div>
+  );
+};
+
+export default Login;
