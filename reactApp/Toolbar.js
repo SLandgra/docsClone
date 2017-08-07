ad5d784513f0ffcf7438726eed7f221a468b9d7b
tlnguyen4 <tlnguyen@princeton.edu>
2017-08-07 13:56:35 -0700

    finish the doc look.

--- reactApp/Toolbar.js
+++ reactApp/Toolbar.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Glyphicon, Button } from 'react-bootstrap';
+import { Glyphicon, Button, ButtonGroup } from 'react-bootstrap';
 
 const Toolbar = ({ onBoldClick, onItalicClick, onULClick, onStrikeClick, onLeftAlignClick }) => {
   return (
     <div style={{overflow: 'hidden', width: '100%', backgroundColor: '#e9e9e9', padding: '20px'}}>
-      <button onClick={() => onBoldClick()}>
-        <Glyphicon glyph="bold"></Glyphicon>
-      </button>
-      <button onClick={() => onItalicClick()}><i>I</i></button>
-      <button onClick={() => onULClick()}><ins>U</ins></button>
-      <button onClick={() => onStrikeClick()}><strong><del>S</del></strong></button>
-      <button onClick={() => onLeftAlignClick()}>Left Align</button>
+      <ButtonGroup style={{marginRight: '5px'}}>
+        <Button onClick={() => onBoldClick()}>
+          <Glyphicon glyph="bold"></Glyphicon>
+        </Button>
+        <Button onClick={() => onItalicClick()}>
+          <Glyphicon glyph="italic"></Glyphicon>
+        </Button>
+        <Button onClick={() => onULClick()}>
+          <Glyphicon glyph="text-underline"></Glyphicon>
+        </Button>
+        <Button onClick={() => onStrikeClick()}>
+          <Glyphicon glyph="strike"></Glyphicon>
+        </Button>
+      </ButtonGroup>
+      <ButtonGroup style={{marginRight: '5px'}}>
+        <Button onClick={() => onLeftAlignClick()}>
+          <Glyphicon glyph="align-left"></Glyphicon>
+        </Button>
+        <Button onClick={() => onLeftAlignClick()}>
+          <Glyphicon glyph="align-center"></Glyphicon>
+        </Button>
+        <Button onClick={() => onLeftAlignClick()}>
+          <Glyphicon glyph="align-right"></Glyphicon>
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
