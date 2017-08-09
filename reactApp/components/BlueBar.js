cb7528fa5af32685797675726069c1d47b62be23
tlnguyen4 <tlnguyen@princeton.edu>
2017-08-08 17:58:11 -0700

    working on log out button

--- reactApp/components/BlueBar.js
+++ reactApp/components/BlueBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 class BlueBar extends React.Component {
@@ -13,12 +13,19 @@ class BlueBar extends React.Component {
   onSearchChange(e) {
     this.setState({search: e.target.value});
   }
+
+  // onLogOutClick() {
+  //   localStorage.removeItem('user_id');
+  //   localStorage.removeItem('password');
+  //   <Redirect to="/" />;
+  // }
+
   render() {
     return (
       <div style={{display: 'flex', position: 'fixed', backgroundColor: '#4285f4', width: '100%', justifyContent: 'space-around'}}>
         <h3 style={{fontFamily: 'Arial', color: 'white', padding: '10px 10px 10px 100px'}}>DOCUMENT PORTAL</h3>
         <input style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px', width: '600px', backgroundColor: '#80aaff', color: 'aliceblue'}} value={this.state.search} placeholder="Search" />
-        <Button style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px'}}><Link to="/login">Log Out</Link></Button>
+        <Button style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px'}} onClick={() => this.onLogOutClick()}>Log Out</Button>
       </div>
     );
   }
