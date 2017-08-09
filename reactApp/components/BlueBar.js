af4f65cdd4245baeea0ef74329e8f86299253412
tlnguyen4 <tlnguyen@princeton.edu>
2017-08-08 18:20:29 -0700

    log out button working

--- reactApp/components/BlueBar.js
+++ reactApp/components/BlueBar.js
@@ -6,7 +6,8 @@ class BlueBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      seach: ''
+      seach: '',
+      redirect: false,
     };
   }
 
@@ -14,20 +15,25 @@ class BlueBar extends React.Component {
     this.setState({search: e.target.value});
   }
 
-  // onLogOutClick() {
-  //   localStorage.removeItem('user_id');
-  //   localStorage.removeItem('password');
-  //   <Redirect to="/login" />;
-  // }
+  onLogOutClick(e) {
+    e.preventDefault();
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('password');
+    this.setState({redirect: true});
+  }
 
   render() {
-    return (
-      <div style={{display: 'flex', position: 'fixed', backgroundColor: '#4285f4', width: '100%', justifyContent: 'space-around'}}>
-        <h3 style={{fontFamily: 'Arial', color: 'white', padding: '10px 10px 10px 100px'}}>DOCUMENT PORTAL</h3>
-        <input style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px', width: '600px', backgroundColor: '#80aaff', color: 'aliceblue'}} value={this.state.search} placeholder="Search" />
-        <Button style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px'}}>Log Out</Button>
-      </div>
-    );
+    if (this.state.redirect) {
+      return <Redirect to="/login" />;
+    } else {
+      return (
+        <div style={{display: 'flex', position: 'fixed', backgroundColor: '#4285f4', width: '100%', justifyContent: 'space-around'}}>
+          <h3 style={{fontFamily: 'Arial', color: 'white', padding: '10px 10px 10px 100px'}}>DOCUMENT PORTAL</h3>
+          <input style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px', width: '600px', backgroundColor: '#80aaff', color: 'aliceblue'}} value={this.state.search} placeholder="Search" />
+          <Button style={{border: '0px', padding: '10px', margin: '10px 0px 10px 15px', borderRadius: '5px'}} onClick={(e) => this.onLogOutClick(e)}>Log Out</Button>
+        </div>
+      );
+    }
   }
 }
 
